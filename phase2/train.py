2f850eb934f50ecd72bb4791b5e8b25c30a6fe95
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-11-14 18:32:21 +0700

    update train

--- phase2/train.py
+++ phase2/train.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.utils.data as data
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from torch import optim
@@ -20,7 +21,7 @@ def get_dataloader(args):
         dataset_test, batch_size=args.test_batch_size, shuffle=False, num_workers=int(args.threads))
 
     return dataloader_train, dataloader_test
-
+           
 def evaluate_model(model, dataloader_test):
     with torch.no_grad():
         model.eval()
@@ -60,9 +61,23 @@ def evaluate_model(model, dataloader_test):
 
         # print("sketch_array_tests[0].shape", sketch_array_tests[0].shape) #(25, 2048)
         num_steps = len(sketch_array_tests[0])
+        avererage_area = []
+        avererage_area_percentile = []
+        mean_rank_ourB = []
+        mean_rank_ourA = []
+        avererage_ourB = []
+        avererage_ourA = []
+        exps = np.linspace(1, num_steps, num_steps) / num_steps
+        factor = np.exp(1 - exps) / np.e
+        sketch_range = []
+        
         rank_all = torch.zeros(len(sketch_array_tests), num_steps)
+        rank_all_percentile = torch.zeros(len(sketch_array_tests), num_steps)
+        sketch_range = torch.Tensor(sketch_range)
         
         for i_batch, sampled_batch in enumerate(sketch_array_tests):
+            mean_rank = []
+            mean_rank_percentile = []
             sketch_name = sketch_names[i_batch]
 
             sketch_query_name = '_'.join(
@@ -78,13 +93,32 @@ def evaluate_model(model, dataloader_test):
                 distance = F.pairwise_distance(sketch_feature.unsqueeze(0).to(device), image_array_tests.to(device))
                 
                 rank_all[i_batch, i_sketch] = distance.le(target_distance).sum()
+                rank_all_percentile[i_batch, i_sketch] = (len(distance) - rank_all[i_batch, i_sketch]) / (len(distance) - 1)
+                
+                if rank_all[i_batch, i_sketch].item() == 0:
+                    mean_rank.append(1.)
+                else:
+                    mean_rank.append(1/rank_all[i_batch, i_sketch].item())
+                    # 1/(rank)
+                    mean_rank_percentile.append(rank_all_percentile[i_batch, i_sketch].item())
+                    mean_rank_ourB.append(1/rank_all[i_batch, i_sketch].item() * factor[i_sketch])
+                    mean_rank_ourA.append(rank_all_percentile[i_batch, i_sketch].item()*factor[i_sketch])
+                    
+            avererage_area.append(np.sum(mean_rank)/len(mean_rank))
+            avererage_area_percentile.append(np.sum(mean_rank_percentile)/len(mean_rank_percentile))
+            avererage_ourB.append(np.sum(mean_rank_ourB)/len(mean_rank_ourB))
+            avererage_ourA.append(np.sum(mean_rank_ourA)/len(mean_rank_ourA))
 
         top1_accuracy = rank_all[:, -1].le(1).sum().numpy() / rank_all.shape[0]
         top5_accuracy = rank_all[:, -1].le(5).sum().numpy() / rank_all.shape[0]
         top10_accuracy = rank_all[:, -1].le(10).sum().numpy() / rank_all.shape[0]
 
+        meanMA = np.mean(avererage_area_percentile)
+        meanMB = np.mean(avererage_area)
+        meanOurB = np.mean(avererage_ourB)
+        meanOurA = np.mean(avererage_ourA)
 
-        return top1_accuracy, top5_accuracy, top10_accuracy
+        return top1_accuracy, top5_accuracy, top10_accuracy, meanMA, meanMB, meanOurA, meanOurB
 
 def train_model(model, args):
     model = model.to(device)
@@ -142,7 +176,7 @@ def train_model(model, args):
         avg_loss = sum(losses) / len(losses)
 
                 
-        top1_eval, top5_eval, top10_eval = evaluate_model(model=model, dataloader_test=dataloader_test)
+        top1_eval, top5_eval, top10_eval, meanA, meanB, meanOurA, meanOurB = evaluate_model(model=model, dataloader_test=dataloader_test)
         if top5_eval > top5:
             top5 = top5_eval
             torch.save(model.state_dict(), "best_top5_model.pth")
@@ -154,7 +188,11 @@ def train_model(model, args):
         print('Top 1 accuracy : {:.5f}'.format(top1_eval))
         print('Top 5 accuracy : {:.5f}'.format(top5_eval))
         print('Top 10 accuracy: {:.5f}'.format(top10_eval))
+        print('Mean A         : {:.5f}'.format(meanA))
+        print('Mean B         : {:.5f}'.format(meanB))
+        print('meanOurA       : {:.5f}'.format(meanOurA))
+        print('meanOurB       : {:.5f}'.format(meanOurB))
         print('Loss           : {:.5f}'.format(avg_loss))
         with open("results_log.txt", "a") as f:
-            f.write("Epoch {:d} | Top1: {:.5f} | Top5: {:.5f} | Top10: {:.5f}\n".format(
-                i_epoch+1, top1_eval, top5_eval, top10_eval))
+            f.write("Epoch {:d} | Top1: {:.5f} | Top5: {:.5f} | Top10: {:.5f} | MeanA: {:.5f} | MeanB: {:.5f} | meanOurA: {:.5f} | meanOurB: {:.5f} \n".format(
+                i_epoch+1, top1_eval, top5_eval, top10_eval, meanA, meanB, meanOurA, meanOurB))
